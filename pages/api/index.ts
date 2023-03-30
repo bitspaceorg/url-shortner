7dca0838c6317f472bc109d6bbd59ae9844a7752
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-03-30 13:05:07 +0530

    prisma intergration w/o spl chars ✨

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/libs/prisma";
 const handler = async (req,res) => {
     if(req.method==='POST'){
         console.log(req.body.toShort);
-        const ra = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!#$%^&*)("
+        const ra = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
         let gen = ""
         for(let i = 0 ; i<5;i++)gen+=ra[Math.floor(Math.random() * ra.length)];
         
