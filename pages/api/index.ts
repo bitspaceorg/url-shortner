689d5f7440762a08b71cdc6a2228160bf8afe34e
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-03-30 12:53:47 +0530

    prisma intergration ✨

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,10 +2,14 @@ import { prisma } from "@/libs/prisma";
 const handler = async (req,res) => {
     if(req.method==='POST'){
         console.log(req.body.toShort);
+        const ra = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!#$%^&*)("
+        let gen = ""
+        for(let i = 0 ; i<5;i++)gen+=ra[Math.floor(Math.random() * ra.length)];
+        
     const data = await prisma.urls.create({
         data : {
             short : req.body.toShort,
-            long : "wthsdfssdddr"
+            long : gen
         }
     });
     res.json(data)
