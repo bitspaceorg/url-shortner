6a66c2f5082ac82617b1d1e78b693509f2e52965
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-05-05 11:26:24 +0530

    Minor bug fix

--- pages/[url].tsx
+++ pages/[url].tsx
@@ -1,37 +1,43 @@
 import { useEffect, useState } from "react";
-import { prisma } from "../libs/prisma"
-import { useRouter } from "next/router"
+import { prisma } from "../libs/prisma";
+import { useRouter } from "next/router";
 
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-export default function Redirect (p:InferGetServerSidePropsType<typeof getServerSideProps>){
+export default function Redirect( p: InferGetServerSidePropsType<typeof getServerSideProps> ) {
 
-    const [isValid, setValid] = useState<boolean>(false);
+  const [isValid, setValid] = useState<boolean>(false);
 
-	useEffect(()=>{
-        if(!(p.data==null)){
-          setValid(true);
-		  window.location.replace("https://"+p.data.long);
-        }else{
-            setValid(false);
-            window.location.replace("http://localhost:3000")
-        }
-	}, []);
-	return (
+  useEffect(() => {
+    if (!(p.data == null)) {
+      setValid(true);
+      window.location.replace("https://" + p.data.long);
+    } else {
+      setValid(false);
+    }
+  }, []);
+
+  return (
     <div className="w-screen h-screen">
-        {isValid ? <div>Redirecting {p.data.long}</div> : <div className="flex flex-col items-center justify-center w-full h-full">The link you entered is not available</div>}
+      {isValid ? (
+        <div>Redirecting {p.data.long}</div>
+      ) : (
+        <div className="flex flex-col items-center justify-center w-full h-full">
+          The link you entered is not available
         </div>
-	)
+      )}
+    </div>
+  );
 }
 
-export const getServerSideProps : GetServerSideProps = async({res, req, resolvedUrl}) => {
-	const url = resolvedUrl.slice(1);
-	const data = await prisma.urls.findUnique({
-		where : {
-			short :url ,
-		}
-	})
-	return {
-	  props: {data}, 
-	}
-  }
+export const getServerSideProps: GetServerSideProps = async ({ res, req, resolvedUrl }) => {
+  const url = resolvedUrl.slice(1);
+  const data = await prisma.urls.findUnique({
+    where: {
+      short: url,
+    },
+  });
+  return {
+    props: { data },
+  };
+};
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,38 +2,43 @@ import { prisma } from "@/libs/prisma";
 import { random_variable_generate } from "@/libs/random_var";
 import axios from "axios";
 
-const handler = async (req,res) => {
-    if(req.method==='POST'){
-        let input = req.body.toShort
-        if ( input.slice(0,7) == 'http://' ) input = input.slice(7)
-        if ( input.slice(0,8) == 'https://' ) input = input.slice(8)
-        await axios.get('https://'+input)
-            .then (async (response) =>{
-                if ( response.status == 200 ) {
-                const v = await prisma.urls.findUnique({
-                        where: {
-                            long: input
-                        }
-                    })
-                if ( v === undefined || v === null ) {
-                    const data = await prisma.urls.create({
-                        data : {
-                            short : random_variable_generate(5),
-                            long : input
-                        }
-                    });
-                    res.json(data)
-                    }
-                else {
-                    res.json({
-                        short: v.short
-                    })
-                }
-                }    
-                    })
-                    .catch ( e => {
-                        res.send({status:404})
-                    })
-                }
-}
+const handler = async (req, res) => {
+
+  if (req.method === "POST") {
+    let input = req.body.toShort;
+    if (input.slice(0, 7) == "http://") input = input.slice(7);
+    if (input.slice(0, 8) == "https://") input = input.slice(8);
+
+    await axios
+      .get("https://" + input)
+      .then(async (response) => {
+
+        if (response.status == 200) {
+          const v = await prisma.urls.findUnique({
+            where: {
+              long: input,
+            },
+          });
+          if (v === undefined || v === null) {
+            const data = await prisma.urls.create({
+              data: {
+                short: random_variable_generate(5),
+                long: input,
+              },
+            });
+            res.json(data);
+          } else {
+            res.json({
+              short: v.short,
+            });
+          }
+        }
+      })
+      .catch((e) => {
+        res.send({ status: 404 });
+      });
+
+  }
+
+};
 export default handler;
