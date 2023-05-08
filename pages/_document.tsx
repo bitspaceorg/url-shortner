d5c31cfd7697605150a335dae3c81a1519aad165
Bharath Sanjeevi T <119755499+BharathSanjeeviT@users.noreply.github.com>
2023-05-08 13:00:21 +0530

    Enhancing Open-Source URL Shortener: Colorful Error Alerts and Bug Fixes (#13)
    * Added readme.md
    
    * Added timer feature

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document() {
           className="mt-5"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
       </Head>
       <body>
         <Main />
