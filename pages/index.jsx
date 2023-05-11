fa9812ef709fdd7dfd85c8d8beed8f5033cb642c
MKMukeshKannan <mukeshkannan311@gmail.com>
2023-05-11 19:24:37 +0530

    Fixed the UI Bug and Refactored the code

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,115 +6,98 @@ export default function Home() {
   const [input, setInput] = useState("");
   const [copy, setCopy] = useState("");
   const [isLoading, setLoading] = useState(false);
-  const [col, setCol] = useState("");
+  const [error, setError] = useState("");
 
   const generateShort = async (e) => {
     e.preventDefault();
-    if (input.includes("urlshortener.bitspace.org.in/")) {
-      console.log("Logged Out");
-      setCol("Already Shortened");
+    if (input.includes("urlshortener.bitspace.org.in")) {
+      setError("Already Shortened");
       setTimeout(() => {
-        setCol("");
+        setError("");
       }, 1000);
       setInput("");
+      setCopy("");
+      setLoading(false);
       return;
     }
     if (input != "") {
-      setCol("");
+      setError("");
       setCopy("");
       setLoading(true);
       const response = await axios.post("/api", {
         toShort: input,
       });
       if (response.data.status === 404) {
-        setCol("Not Found");
+        setError("Not Found");
         setTimeout(() => {
-          setCol("");
+          setError("");
         }, 1000);
         setCopy("");
         setLoading(false);
       } else {
-        setCol("");
+        setError("");
         setLoading(false);
         setCopy(response.data.short);
       }
       setInput("");
     } else {
       setCopy("");
-      setCol("Enter Something");
+      setError("Enter Something");
       setTimeout(() => {
-        setCol("");
+        setError("");
       }, 1000);
     }
   };
 
+  let color = error === "" ? "black" : "red-500";
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen bg-white">
-      <form
-        onSubmit={(e) => generateShort(e)}
-        className="flex flex-col justify-center items-center"
-      >
-        {col == "" ? (
-          <div className="flex flex-row flex-wrap justify-center items-center">
-            <input
-              placeholder="Enter your url *here*"
-              className="w-[270px]  h-[50px] border-4 border-black px-2 outline-none text-xl"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-            <button
-              type="submit"
-              className="bg-black h-[50px] text-white px-2 font-[600]  text-xl"
-            >
-              Shorten
-            </button>
-          </div>
-        ) : (
-          <div className="flex flex-col  justify-center items-center">
-            <div className="flex flex-row flex-wrap justify-center items-center">
-              <input
-                placeholder="Enter your url *here*"
-                className="w-[270px]  h-[50px] border-4 border-red-500 px-2 outline-none text-xl"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-              />
-              <button
-                type="submit"
-                className="bg-red-500 h-[50px] text-white px-2 font-[600] text-xl "
-              >
-                Shorten
-              </button>
-            </div>
-            <br />
-            <div className="text-red-500 mt-2">
-              <i className="mr-2 text-red-500 align-middle material-symbols-outlined">
-                error
-              </i>
-              {col}
-            </div>
-          </div>
-        )}
-        <style jsx>
-          {`
-            @media screen and (max-width: 358px) {
-              button {
-                width: 270px;
-              }
-            }
-          `}
-        </style>
+      <form onSubmit={(e) => generateShort(e)}>
+        <div className="flex flex-row flex-wrap justify-center items-center">
+          <input
+            placeholder="Enter your url *here*"
+            className={`w-[270px]  h-[50px] border-4 border-${color} px-2 outline-none text-xl`}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button
+            type="submit"
+            className={`bg-${color} h-[50px] text-white px-2 font-[600] text-xl`}
+          >
+            Shorten
+          </button>
+        </div>
       </form>
-      <div className="text-2xl font-[600] m-4">
-        {isLoading && <h1>Loading...</h1>}
-        {copy.length === 0 || !(col === "") ? null : (
-          <div>
-            <h1
-              onClick={() => navigator.clipboard.writeText(`${HOST}/${copy}`)}
-              className="text-3xl font-black cursor-pointer text-center mt-4"
-            >
-              {copy} <i className="font-black material-icons">&#xe3e0;</i>
-            </h1>
-          </div>
+
+      <style jsx>
+        {`
+          @media screen and (max-width: 358px) {
+            button {
+              width: 270px;
+            }
+          }
+        `}
+      </style>
+
+      <div className="h-6 font-[600] m-4">
+        {isLoading && (
+          <h1 className="text-3xl font-black text-center">Loading...</h1>
+        )}
+
+        {error !== "" && (
+          <h1 className="mr-2 text-red-500 ">
+            <i className="align-middle material-symbols-outlined">error</i>{" "}
+            {error}
+          </h1>
+        )}
+
+        {copy.length === 0 || !(error === "") ? null : (
+          <h1
+            onClick={() => navigator.clipboard.writeText(`${HOST}/${copy}`)}
+            className="text-3xl font-black cursor-pointer text-center"
+          >
+            {copy} <i className="font-black material-icons">&#xe3e0;</i>
+          </h1>
         )}
       </div>
     </div>
